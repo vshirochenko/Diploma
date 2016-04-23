bad72632ae7e90ff427edb66a94894eba8245eab
vshirochenko <shirochenkovasya011@mail.ru>
2016-04-23 18:23:17 +0200

    Added generic repository

---
+++ Diploma/DAL/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        // Получаем все элементы из базы
+        IEnumerable<T> SelectAll();
+        // Получаем конкретный элемент по признаку
+        T SelectByID(object id);
+        // Вставляем элемент
+        void Insert(T entity);
+        // Обновляем элемент
+        void Update(T entity);
+        // Удаляем элемент
+        void Delete(T entity);
+        // Сохраняем изменения
+        void Save();
+    }
+}
