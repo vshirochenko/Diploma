435b7aeeb7df6d36c7d737975a479cf87d1b9016
vshirochenko <shirochenkovasya011@mail.ru>
2016-04-23 17:49:03 +0200

    Added dbContext

---
+++ Diploma/DAL/MunicipalityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Diploma.Models;
+
+namespace Diploma.DAL
+{
+    public class MunicipalityContext : DbContext
+    {
+        public MunicipalityContext() : base("MunicipalityContext") { }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
